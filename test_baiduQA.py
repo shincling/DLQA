ad37574ee48890a6b9c94024cf7f6a61369b5632
shincling <shijing609@163.com>
2017-09-01 12:26:57 +0800

    finished the data.

--- test_baiduQA.py
+++ test_baiduQA.py
@@ -2,10 +2,13 @@
 import re
 import gzip
 cc=gzip.open('/media/shin/新加卷1/DeepLearning/数据集/WebQA.v1.0/data/test.ann.json.gz','r').read()
-print len(cc)
+# cc=gzip.open('/media/shin/新加卷1/DeepLearning/数据集/WebQA.v1.0/data/training.json.gz','r').read()
 
 ccc=cc.split('{"q_key')[1:]
+print len(ccc)
 
+# raise EOFError
+output=open('output','w')
 for cc in ccc:
     print '\n\n'
     sent=re.findall('evidence_tokens": \[(.*?)\],',cc)[0].split(',')
@@ -14,12 +17,15 @@ for cc in ccc:
 
     print 'Sent: ',
     for i in sent:
-        print i.strip()[1:-1].encode("utf-8").decode('unicode_escape'),
+        print i.strip()[1:-1].encode("utf-8").decode('unicode_escape').encode('utf8'),
+        # print i.strip()[1:-1].decode('utf8'),
     print '\nQuestion: ',
     for i in q:
-        print i.strip()[1:-1].encode("utf-8").decode('unicode_escape'),
+        print i.strip()[1:-1].encode("utf-8").decode('unicode_escape').encode('utf8'),
+        # print i.strip()[1:-1].encode("utf-8"),
     print '\nAnswer: ',
     for i in a:
-        print i.strip()[1:-1].encode("utf-8").decode('unicode_escape'),
+        print i.strip()[1:-1].encode("utf-8").decode('unicode_escape').encode('utf8'),
+        # print i.strip()[1:-1].encode("utf-8"),
 
 
